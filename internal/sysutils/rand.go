83addc48d4a610eefd51cff9e4d50b6d2babee0c
erodrigufer <erodrigufer@gmail.com>
2022-11-07 02:43:57 +0100

    add explanations about type conversion and crypto/rand package usage

--- internal/sysutils/rand.go
+++ internal/sysutils/rand.go
@@ -14,7 +14,11 @@ func NewRandomString(length int, charset string) (string, error) {
 	// Make a slice of length length, in which to store random characters.
 	b := make([]byte, length)
 	for i := range b {
+		// Use the cryptographically more secure implementation rand.Int() to
+		// get a pseudo-random integer (this is more secure than seeding a
+		// pseudo-random generator yourself).
 		r, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		// Convert the big.Int type to a int64 type.
 		r64 := r.Int64()
 		if err != nil {
 			return "", fmt.Errorf("error: getting a random integer failed: %w", err)
@@ -37,8 +41,7 @@ func NewRandomPassword(length int, charset string) (string, error) {
 	return s, nil
 }
 
-// NewRandomUsername, returns a random password of the given character
-// length.
+// NewRandomUsername, returns a random username of the given character length.
 func NewRandomUsername(length int, charset string) (string, error) {
 	s, err := NewRandomString(length, charset)
 	if err != nil {
